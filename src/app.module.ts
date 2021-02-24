b727c1e63e44d739753b32070bb65f24b1ee6116
Ibra Nurlette <sjsdev02@gmail.com>
2021-02-24 11:20:43 +0700

    handle error

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,21 @@
+import { APP_FILTER } from '@nestjs/core';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { IdeaService } from './database/entities/umum/idea.service';
 import { IdeaModule } from './database/entities/umum/idea.module';
+import { HttpErrorFilter } from './shared/http-error-filter';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), IdeaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpErrorFilter,
+    },
+  ],
 })
 export class AppModule {}

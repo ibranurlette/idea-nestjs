d45772769d5b2932017d2bf702e5b97d8437611a
Ibra Nurlette <sjsdev02@gmail.com>
2021-02-24 14:01:27 +0700

    validation pipe

--- src/database/entities/umum/idea.controller.ts
+++ src/database/entities/umum/idea.controller.ts
@@ -6,13 +6,18 @@ import {
   Delete,
   Body,
   Param,
+  UsePipes,
+  Logger,
 } from '@nestjs/common';
 
 import { IdeaService } from './idea.service';
 import { IdeaDTO } from './idea.dto';
+import { ValidationPipe } from 'src/shared/validation.pipe';
 
 @Controller('idea')
 export class IdeaController {
+  private logger = new Logger('IdeaController');
+
   constructor(private IdeaService: IdeaService) {}
 
   @Get()
@@ -21,7 +26,9 @@ export class IdeaController {
   }
 
   @Post()
+  @UsePipes(new ValidationPipe())
   createIdea(@Body() data: IdeaDTO) {
+    this.logger.log(JSON.stringify(data));
     return this.IdeaService.create(data);
   }
 
@@ -31,7 +38,9 @@ export class IdeaController {
   }
 
   @Put(':id')
+  @UsePipes(new ValidationPipe())
   updateIdea(@Param('id') id: string, @Body() data: Partial<IdeaDTO>) {
+    this.logger.log(JSON.stringify(data));
     return this.IdeaService.update(id, data);
   }
 
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { ValidationPipe } from './shared/validation.pipe';
 
 const port = process.env.PORT || 8080;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
 
   Logger.log(`server running on http://localhost:${port}`, 'bootsrap');
